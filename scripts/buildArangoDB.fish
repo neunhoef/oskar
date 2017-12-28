c9b7b22636fbfbba582626936338ef4c177d00d3
Max Neunhoeffer <max@arangodb.com>
2017-12-28 10:28:01 +0100

    Use gold linker.

--- scripts/buildArangoDB.fish
+++ scripts/buildArangoDB.fish
@@ -8,7 +8,9 @@ cmake -DCMAKE_BUILD_TYPE=$BUILDTYPE \
       -DCMAKE_CXX_COMPILER=/usr/lib/ccache/g++ \
       -DCMAKE_C_COMPILER=/usr/lib/ccache/gcc \
       -DUSE_MAINTAINER_MODE=$MAINTAINER \
-      -DCMAKE_BUILD_TYPE=$BUILDTYPE \
       -DUSE_ENTERPRISE=$ENTERPRISEEDITION \
+      -DCMAKE_EXE_LINKER_FLAGS=-fuse-ld=gold \
+      -DCMAKE_SHARED_LINKER_FLAGS=-fuse-ld=gold \
+      -DUSE_JEMALLOC=On \
       ..
 nice make -j$PARALLELISM
