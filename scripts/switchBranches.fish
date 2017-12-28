2b6b8070ae69d333dd73ae1ee4e9205ba4980946
Max Neunhoeffer <max@arangodb.com>
2017-12-28 10:10:47 +0100

    Reset worktree before branch change.

--- scripts/switchBranches.fish
+++ scripts/switchBranches.fish
@@ -1,5 +1,9 @@
 #!/usr/bin/fish
 cd /work/ArangoDB
+git checkout -- .
 git checkout $argv[1]
-cd enterprise
-git checkout $argv[2]
+if test $ENTERPRISEEDITION = On
+  cd enterprise
+  git checkout -- .
+  git checkout $argv[2]
+end
