8abd8fc4ae2f4d0c52bda1d7ff99793536a3fa87
Max Neunhoeffer <max@arangodb.com>
2017-12-28 11:29:54 +0100

    Forgot git pull

--- scripts/switchBranches.fish
+++ scripts/switchBranches.fish
@@ -2,8 +2,10 @@
 cd /work/ArangoDB
 git checkout -- .
 git checkout $argv[1]
+git pull
 if test $ENTERPRISEEDITION = On
   cd enterprise
   git checkout -- .
   git checkout $argv[2]
+  git pull
 end
