276c756c5a87826aec174981bd6d4b1c8df8e2f0
Max Neunhoeffer <max@arangodb.com>
2018-01-10 10:30:14 +0100

    Double pull.

--- scripts/switchBranches.fish
+++ scripts/switchBranches.fish
@@ -1,11 +1,13 @@
 #!/usr/bin/fish
 cd $INNERWORKDIR/ArangoDB
 git checkout -- .
+git pull
 git checkout $argv[1]
 git pull
 if test $ENTERPRISEEDITION = On
   cd enterprise
   git checkout -- .
+  git pull
   git checkout $argv[2]
   git pull
 end
