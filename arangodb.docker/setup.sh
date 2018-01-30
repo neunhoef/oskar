62de60941507e98c8e4e93b96035a76ca418d631
Max Neunhoeffer <max@arangodb.com>
2018-01-31 00:38:38 +0100

    Setup users and dirs.

---
+++ arangodb.docker/setup.sh
@@ -0,0 +1,9 @@
+#!/bin/sh
+getent group arangodb > /dev/null || addgroup -S arangodb
+getent passwd arangodb > /dev/null || adduser -S -G arangodb -D -h /usr/shar
+e/arangodb3 -H -s /bin/false -g "ArangoDB Application User" arangodb
+
+install -o arangodb -g arangodb -m 755 -d /var/lib/arangodb3
+install -o arangodb -g arangodb -m 755 -d /var/lib/arangodb3-apps
+install -o arangodb -g arangodb -m 755 -d /var/log/arangodb3
+
