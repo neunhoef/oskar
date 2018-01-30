bd930f8cbec6653fec4708829ec0bc0e84e5ece5
Max Neunhoeffer <max@arangodb.com>
2018-01-31 00:40:48 +0100

    Fix line break.

--- arangodb.docker/setup.sh
+++ arangodb.docker/setup.sh
@@ -1,7 +1,6 @@
 #!/bin/sh
 getent group arangodb > /dev/null || addgroup -S arangodb
-getent passwd arangodb > /dev/null || adduser -S -G arangodb -D -h /usr/shar
-e/arangodb3 -H -s /bin/false -g "ArangoDB Application User" arangodb
+getent passwd arangodb > /dev/null || adduser -S -G arangodb -D -h /usr/share/arangodb3 -H -s /bin/false -g "ArangoDB Application User" arangodb
 
 install -o arangodb -g arangodb -m 755 -d /var/lib/arangodb3
 install -o arangodb -g arangodb -m 755 -d /var/lib/arangodb3-apps
