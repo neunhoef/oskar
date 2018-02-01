18de0b892adcee678d25e610e5fcabb98fb25de3
Max Neunhoeffer <max@arangodb.com>
2018-02-01 11:21:28 +0100

    Debugging output.

--- buildUbuntu.docker/scripts/switchBranches.fish
+++ buildUbuntu.docker/scripts/switchBranches.fish
@@ -1,14 +1,24 @@
 #!/usr/bin/fish
+echo $SSH_AUTH_SOCK
 cd $INNERWORKDIR/ArangoDB
+echo Hallo1
 git checkout -- .
+echo Hallo2
 git pull
+echo Hallo3
 git checkout $argv[1]
+echo Hallo4
 git pull
+echo Hallo5
 if test $ENTERPRISEEDITION = On
   cd enterprise
   git checkout -- .
+echo Hallo6
   git pull
+echo Hallo7
   git checkout $argv[2]
+echo Hallo8
   git pull
+echo Hallo9
 end
 chown -R $UID:$GID $INNERWORKDIR
