cabfc671e8a8a7e87c288c3ab21b999e7647d422
Max Neunhoeffer <max@arangodb.com>
2018-02-12 14:54:13 +0100

    Add pipetest.

---
+++ jenkins/pipetest.fish
@@ -0,0 +1,9 @@
+#!/usr/bin/fish
+env
+function term -s TERM ; echo %self got term ; echo %self got term >>$HOME/fishpids ; end
+echo Los %self
+echo Los %self >>$HOME/fishpids
+sleep 180
+echo Fertig %self
+echo Fertig %self >> $HOME/fishpids
+
