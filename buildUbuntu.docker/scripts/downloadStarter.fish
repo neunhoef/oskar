a70469e46a6638e97dfcdcb020acf0cc7524ed76
Max Neunhoeffer <max@arangodb.com>
2018-02-13 16:27:19 +0100

    Allow starter and syncer version to be "latest".

--- buildUbuntu.docker/scripts/downloadStarter.fish
+++ buildUbuntu.docker/scripts/downloadStarter.fish
@@ -11,6 +11,12 @@ if test "$argv[1]" = ""
 else
   set STARTER_REV "$argv[1]"
 end
+if test "$STARTER_REV" = latest
+  set -l meta (curl -s -L "https://api.github.com/repos/arangodb-helper/arangodb/releases/latest")
+  or begin ; echo "Finding download asset failed for latest" ; exit 1 ; end
+  set STARTER_REV (echo $meta | jq -r ".name")
+  or begin ; echo "Could not parse downloaded JSON" ; exit 1 ; end
+end
 echo Using STARTER_REV "$STARTER_REV"
 
 curl -s -L -o "$INNERWORKDIR/ArangoDB/build/install/usr/bin/arangodb" "https://github.com/arangodb-helper/arangodb/releases/download/$STARTER_REV/arangodb-linux-amd64"
--- buildUbuntu.docker/scripts/downloadSyncer.fish
+++ buildUbuntu.docker/scripts/downloadSyncer.fish
@@ -17,16 +17,19 @@ if test (count $argv) = 0
 else
   set SYNCER_REV "$argv[1]"
 end
+if test "$SYNCER_REV" = latest
+  set -l meta (curl -s -L "https://$DOWNLOAD_SYNC_USER@api.github.com/repos/arangodb/arangosync/releases/latest")
+  or begin ; echo "Finding download asset failed for latest" ; exit 1 ; end
+  set SYNCER_REV (echo $meta | jq -r ".name")
+  or begin ; echo "Could not parse downloaded JSON" ; exit 1 ; end
+end
 
 echo Using DOWNLOAD_SYNC_USER "$DOWNLOAD_SYNC_USER"
 echo Using SYNCER_REV "$SYNCER_REV"
 
 # First find the assets and linux executable:
 set -l meta (curl -s -L https://$DOWNLOAD_SYNC_USER@api.github.com/repos/arangodb/arangosync/releases/tags/$SYNCER_REV)
-if test $status != 0
-  echo Finding download asset failed
-  exit 1
-end
+or begin ; echo Finding download asset failed ; exit 1 ; end
 
 echo $meta > $INNERWORKDIR/assets.json
 
