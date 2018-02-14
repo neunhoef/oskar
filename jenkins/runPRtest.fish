6b2735f27f8050b601ec6a505a16dd4e54b833a8
Max Neunhoeffer <max@arangodb.com>
2018-02-14 23:02:58 +0100

    Another fix for script.

--- jenkins/runPRtest.fish
+++ jenkins/runPRtest.fish
@@ -1,16 +1,18 @@
 #!/usr/bin/fish
 cd $HOME
-if cd oskar ^ /dev/null ; git pull
-else ; git clone https://github.com/neunhoef/oskar ; and cd oskar ; end
+if not cd oskar ^ /dev/null 
+  git clone https://github.com/neunhoef/oskar ; and cd oskar
+end
 and source helper.fish
 if test $status != 0 ; echo Did not find oskar and helpers ; exit 1 ; end
 
-updateOskar ; lockDirectory ; clearResults
+lockDirectory ; updateOskar ; clearResults
 
 eval $EDITION ; eval $STORAGE_ENGINE ; eval $TEST_SUITE
 
 switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH
 and oskar1
 
-set -l s $status ; moveResultsToWorkspace ; unlockDirectory ; exit $s
+set -l s $status ; moveResultsToWorkspace ; unlockDirectory 
+exit $s
 
