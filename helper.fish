acca7aecf5a45ecccdade2d43b1301824d257ce0
Michael Hackstein <michael@arangodb.com>
2018-02-15 13:41:02 +0100

    Moved linux specific part from helper.fish to helper.linux.fish

--- helper.fish
+++ helper.fish
@@ -1,7 +1,3 @@
-set -xg UBUNTUBUILDIMAGE neunhoef/ubuntubuildarangodb
-set -xg UBUNTUPACKAGINGIMAGE neunhoef/ubuntupackagearangodb
-set -xg ALPINEBUILDIMAGE neunhoef/alpinebuildarangodb
-
 function lockDirectory
   # Now grab the lock ourselves:
   set -l pid (echo %self)
@@ -76,85 +72,6 @@ set -g INNERWORKDIR /work
 if test ! -d work ; mkdir work ; end
 set -g VERBOSEOSKAR Off
 
-function buildUbuntuBuildImage
-  cd $WORKDIR/buildUbuntu.docker
-  docker build -t $UBUNTUBUILDIMAGE .
-  cd $WORKDIR
-end
-function pushUbuntuBuildImage ; docker push $UBUNTUBUILDIMAGE ; end
-function pullUbuntuBuildImage ; docker pull $UBUNTUBUILDIMAGE ; end
-
-function buildUbuntuPackagingImage
-  cd $WORKDIR/buildUbuntuPackaging.docker
-  docker build -t $UBUNTUPACKAGINGIMAGE .
-  cd $WORKDIR
-end
-function pushUbuntuPackagingImage ; docker push $UBUNTUPACKAGINGIMAGE ; end
-function pullUbuntuPackagingImage ; docker pull $UBUNTUPACKAGINGIMAGE ; end
-
-function buildAlpineBuildImage
-  cd $WORKDIR/buildAlpine.docker
-  docker build -t $ALPINEBUILDIMAGE .
-  cd $WORKDIR
-end
-function pushAlpineBuildImage ; docker push $ALPINEBUILDIMAGE ; end
-function pullAlpineBuildImage ; docker pull $ALPINEBUILDIMAGE ; end
-
-function remakeImages
-  buildUbuntuBuildImage
-  pushUbuntuBuildImage
-  buildAlpineBuildImage
-  pushAlpineBuildImage
-  buildUbuntuPackagingImage
-  pushUbuntuPackagingImage
-end
-
-function runInContainer
-  if test -z "$SSH_AUTH_SOCK"
-    eval (ssh-agent -c) > /dev/null
-    ssh-add ~/.ssh/id_rsa
-    set -l agentstarted 1
-  else
-    set -l agentstarted ""
-  end
-  docker run -v $WORKDIR/work:$INNERWORKDIR \
-             -v $SSH_AUTH_SOCK:/ssh-agent \
-             -e SSH_AUTH_SOCK=/ssh-agent \
-             -e UID=(id -u) \
-             -e GID=(id -g) \
-             --rm \
-             -e NOSTRIP="$NOSTRIP" \
-             -e GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" \
-             -e INNERWORKDIR=$INNERWORKDIR \
-             -e MAINTAINER=$MAINTAINER \
-             -e BUILDMODE=$BUILDMODE \
-             -e PARALLELISM=$PARALLELISM \
-             -e STORAGEENGINE=$STORAGEENGINE \
-             -e TESTSUITE=$TESTSUITE \
-             -e VERBOSEOSKAR=$VERBOSEOSKAR \
-             -e ENTERPRISEEDITION=$ENTERPRISEEDITION \
-             $argv
-  set -l s $status
-  if test -n "$agentstarted"
-    ssh-agent -k > /dev/null
-    set -e SSH_AUTH_SOCK
-    set -e SSH_AGENT_PID
-  end
-  return $s
-end
-
-function checkoutArangoDB
-  runInContainer $UBUNTUBUILDIMAGE /scripts/checkoutArangoDB.fish
-  or return $status
-  community
-end
-
-function checkoutEnterprise
-  runInContainer $UBUNTUBUILDIMAGE /scripts/checkoutEnterprise.fish
-  or return $status
-  enterprise
-end
-
 function checkoutIfNeeded
   if test ! -d $WORKDIR/ArangoDB
     if test "$ENTERPRISEEDITION" = "On"
@@ -165,95 +82,10 @@ function checkoutIfNeeded
   end
 end
 
-function switchBranches
-  checkoutIfNeeded
-  runInContainer $UBUNTUBUILDIMAGE /scripts/switchBranches.fish $argv
-end
-
-function clearWorkdir
-  runInContainer $UBUNTUBUILDIMAGE /scripts/clearWorkdir.fish
-end
-
 function clearResults
   cd $WORKDIR/work
   for f in testreport* ; rm -f $f ; end
   rm -f test.log
-end
-
-function buildArangoDB
-  checkoutIfNeeded
-  runInContainer $UBUNTUBUILDIMAGE /scripts/buildArangoDB.fish $argv
-  set -l s $status
-  if test $s != 0
-    echo Build error!
-    return $s
-  end
-end
-
-function buildStaticArangoDB
-  checkoutIfNeeded
-  if test ! -d $WORKDIR/ArangoDB
-    if test "$ENTERPRISEEDITION" = "On"
-      checkoutEnterprise
-    else
-      checkoutArangoDB
-    end
-  end
-  runInContainer $ALPINEBUILDIMAGE /scripts/build.fish $argv
-  set -l s $status
-  if test $s != 0
-    echo Build error!
-    return $s
-  end
-end
-
-function buildDebianPackage
-  # This assumes that a static build has already happened
-  # There must be one argument, which is the version number in the
-  # format 3.3.3-1
-  set -l v $argv[1]
-  set -l ch $WORKDIR/work/debian/changelog
-  if test -z "$v"
-    echo Need one version argument in the form 3.3.3-1.
-    return 1
-  end
-
-  cd $WORKDIR
-  rm -rf $WORKDIR/work/debian
-  and if test "$ENTERPRISEEDITION" = "On"
-    echo Building enterprise edition debian package...
-    cp -a debian.enterprise $WORKDIR/work/debian
-    and echo -n "arangodb3e " > $ch
-  else
-    echo Building community edition debian package...
-    cp -a debian.community $WORKDIR/work/debian
-    and echo -n "arangodb3 " > $ch
-  end
-  and echo "($v) UNRELEASED; urgency=medium" >> $ch
-  and echo >> $ch
-  and echo "  * New version." >> $ch
-  and echo >> $ch
-  and echo -n " -- ArangoDB <hackers@arangodb.com>  " >> $ch
-  and date -R >> $ch
-  and runInContainer $UBUNTUPACKAGINGIMAGE /scripts/buildDebianPackage.fish
-  set -l s $status
-  if test $s != 0
-    echo Error when building a debian package
-    return $s
-  end
-end
-
-function shellInUbuntuContainer
-  runInContainer $UBUNTUBUILDIMAGE fish
-end
-
-function shellInAlpineContainer
-  runInContainer $ALPINEBUILDIMAGE fish
-end
-
-function oskar
-  checkoutIfNeeded
-  runInContainer $UBUNTUBUILDIMAGE /scripts/runTests.fish
 end
 
 function oskar1
@@ -303,37 +135,13 @@ function oskar8
   cluster ; rocksdb
 end
 
-function pushOskar
-  cd $WORKDIR
-  source helper.fish
-  git push
-  buildUbuntuBuildImage
-  pushUbuntuBuildImage
-  buildAlpineBuildImage
-  pushAlpineBuildImage
-  buildUbuntuPackagingImage
-  pushUbuntuPackagingImage
-end
-
-function updateOskar
-  cd $WORKDIR
-  git pull
-  source helper.fish
-  pullUbuntuBuildImage
-  pullAlpineBuildImage
-  pullUbuntuPackagingImage
-end
-
 function showLog
   less +G work/test.log
 end
 
-function downloadStarter
-  runInContainer $UBUNTUBUILDIMAGE /scripts/downloadStarter.fish $argv
-end
-
-function downloadSyncer
-  runInContainer -e DOWNLOAD_SYNC_USER=$DOWNLOAD_SYNC_USER $UBUNTUBUILDIMAGE /scripts/downloadSyncer.fish $argv
+switch (uname)
+  case Darwin ; source helper.mac.fish
+  case '*' ; source helper.linux.fish
 end
 
 function findArangoDBVersion
@@ -365,9 +173,7 @@ function makeRelease
   buildStaticArangoDB -DTARGET_ARCHITECTURE=nehalem
   and downloadStarter
   and downloadSyncer
-  and buildDebianPackage $v
-  # and buildRpmPackage $v
-  # and buildDockerImage $v
+  and buildPackage $v
 
   if test $status != 0
     echo Building enterprise release failed, stopping.
@@ -378,8 +184,7 @@ function makeRelease
   buildStaticArangoDB _DTARGET_ARCHITECTURE=nehalem
   and downloadStarter
   and buildDebianPackage $v
-  # and buildRpmPackage $v
-  # and buildDockerImage $v
+  and buildPackage $v
 
   if test $status != 0
     echo Building community release failed.
@@ -393,24 +198,6 @@ function moveResultsToWorkspace
   for f in work/testreport* ; mv $f $WORKSPACE ; end
   for f in work/*.deb ; mv $f $WORKSPACE ; end
   if test -f work/test.log ; mv work/test.log $WORKSPACE ; end
-end
-
-function makeDockerImage
-  if test (count $argv) = 0
-    echo Must give image name as argument
-    return 1
-  end
-  set -l imagename $argv[1]
-
-  cd $WORKDIR/work/ArangoDB/build/install
-  and tar czvf $WORKDIR/arangodb.docker/install.tar.gz *
-  if test $status != 0
-    echo Could not create install tarball!
-    return 1
-  end
-
-  cd $WORKDIR/arangodb.docker
-  docker build -t $imagename .
 end
 
 showConfig
---
+++ helper.linux.fish
@@ -0,0 +1,222 @@
+set -xg SCRIPTSDIR /scripts
+set -xg PLATFORM linux
+set -xg UBUNTUBUILDIMAGE neunhoef/ubuntubuildarangodb
+set -xg UBUNTUPACKAGINGIMAGE neunhoef/ubuntupackagearangodb
+set -xg ALPINEBUILDIMAGE neunhoef/alpinebuildarangodb
+
+function buildUbuntuBuildImage
+  cd $WORKDIR/buildUbuntu.docker
+  docker build -t $UBUNTUBUILDIMAGE .
+  cd $WORKDIR
+end
+function pushUbuntuBuildImage ; docker push $UBUNTUBUILDIMAGE ; end
+function pullUbuntuBuildImage ; docker pull $UBUNTUBUILDIMAGE ; end
+
+function buildUbuntuPackagingImage
+  cd $WORKDIR/buildUbuntuPackaging.docker
+  docker build -t $UBUNTUPACKAGINGIMAGE .
+  cd $WORKDIR
+end
+function pushUbuntuPackagingImage ; docker push $UBUNTUPACKAGINGIMAGE ; end
+function pullUbuntuPackagingImage ; docker pull $UBUNTUPACKAGINGIMAGE ; end
+
+function buildAlpineBuildImage
+  cd $WORKDIR/buildAlpine.docker
+  docker build -t $ALPINEBUILDIMAGE .
+  cd $WORKDIR
+end
+function pushAlpineBuildImage ; docker push $ALPINEBUILDIMAGE ; end
+function pullAlpineBuildImage ; docker pull $ALPINEBUILDIMAGE ; end
+
+function remakeImages
+  buildUbuntuBuildImage
+  pushUbuntuBuildImage
+  buildAlpineBuildImage
+  pushAlpineBuildImage
+  buildUbuntuPackagingImage
+  pushUbuntuPackagingImage
+end
+
+function runInContainer
+  if test -z "$SSH_AUTH_SOCK"
+    eval (ssh-agent -c) > /dev/null
+    ssh-add ~/.ssh/id_rsa
+    set -l agentstarted 1
+  else
+    set -l agentstarted ""
+  end
+  docker run -v $WORKDIR/work:$INNERWORKDIR \
+             -v $SSH_AUTH_SOCK:/ssh-agent \
+             -e SSH_AUTH_SOCK=/ssh-agent \
+             -e UID=(id -u) \
+             -e GID=(id -g) \
+             --rm \
+             -e NOSTRIP="$NOSTRIP" \
+             -e GIT_SSH_COMMAND="ssh -o StrictHostKeyChecking=no" \
+             -e INNERWORKDIR=$INNERWORKDIR \
+             -e MAINTAINER=$MAINTAINER \
+             -e BUILDMODE=$BUILDMODE \
+             -e PARALLELISM=$PARALLELISM \
+             -e STORAGEENGINE=$STORAGEENGINE \
+             -e TESTSUITE=$TESTSUITE \
+             -e VERBOSEOSKAR=$VERBOSEOSKAR \
+             -e ENTERPRISEEDITION=$ENTERPRISEEDITION \
+             $argv
+  set -l s $status
+  if test -n "$agentstarted"
+    ssh-agent -k > /dev/null
+    set -e SSH_AUTH_SOCK
+    set -e SSH_AGENT_PID
+  end
+  return $s
+end
+
+function checkoutArangoDB
+  runInContainer $UBUNTUBUILDIMAGE /scripts/checkoutArangoDB.fish
+  or return $status
+  community
+end
+
+function checkoutEnterprise
+  runInContainer $UBUNTUBUILDIMAGE /scripts/checkoutEnterprise.fish
+  or return $status
+  enterprise
+end
+
+function switchBranches
+  checkoutIfNeeded
+  runInContainer $UBUNTUBUILDIMAGE /scripts/switchBranches.fish $argv
+end
+
+function clearWorkdir
+  runInContainer $UBUNTUBUILDIMAGE /scripts/clearWorkdir.fish
+end
+
+function buildArangoDB
+  checkoutIfNeeded
+  runInContainer $UBUNTUBUILDIMAGE /scripts/buildArangoDB.fish $argv
+  set -l s $status
+  if test $s != 0
+    echo Build error!
+    return $s
+  end
+end
+
+function buildStaticArangoDB
+  checkoutIfNeeded
+  if test ! -d $WORKDIR/ArangoDB
+    if test "$ENTERPRISEEDITION" = "On"
+      checkoutEnterprise
+    else
+      checkoutArangoDB
+    end
+  end
+  runInContainer $ALPINEBUILDIMAGE /scripts/build.fish $argv
+  set -l s $status
+  if test $s != 0
+    echo Build error!
+    return $s
+  end
+end
+
+function buildDebianPackage
+  # This assumes that a static build has already happened
+  # There must be one argument, which is the version number in the
+  # format 3.3.3-1
+  set -l v $argv[1]
+  set -l ch $WORKDIR/work/debian/changelog
+  if test -z "$v"
+    echo Need one version argument in the form 3.3.3-1.
+    return 1
+  end
+
+  cd $WORKDIR
+  rm -rf $WORKDIR/work/debian
+  and if test "$ENTERPRISEEDITION" = "On"
+    echo Building enterprise edition debian package...
+    cp -a debian.enterprise $WORKDIR/work/debian
+    and echo -n "arangodb3e " > $ch
+  else
+    echo Building community edition debian package...
+    cp -a debian.community $WORKDIR/work/debian
+    and echo -n "arangodb3 " > $ch
+  end
+  and echo "($v) UNRELEASED; urgency=medium" >> $ch
+  and echo >> $ch
+  and echo "  * New version." >> $ch
+  and echo >> $ch
+  and echo -n " -- ArangoDB <hackers@arangodb.com>  " >> $ch
+  and date -R >> $ch
+  and runInContainer $UBUNTUPACKAGINGIMAGE /scripts/buildDebianPackage.fish
+  set -l s $status
+  if test $s != 0
+    echo Error when building a debian package
+    return $s
+  end
+end
+
+function shellInUbuntuContainer
+  runInContainer $UBUNTUBUILDIMAGE fish
+end
+
+function shellInAlpineContainer
+  runInContainer $ALPINEBUILDIMAGE fish
+end
+
+function oskar
+  checkoutIfNeeded
+  runInContainer $UBUNTUBUILDIMAGE /scripts/runTests.fish
+end
+
+function pushOskar
+  cd $WORKDIR
+  source helper.fish
+  git push
+  buildUbuntuBuildImage
+  pushUbuntuBuildImage
+  buildAlpineBuildImage
+  pushAlpineBuildImage
+  buildUbuntuPackagingImage
+  pushUbuntuPackagingImage
+end
+
+function updateOskar
+  cd $WORKDIR
+  git pull
+  source helper.fish
+  pullUbuntuBuildImage
+  pullAlpineBuildImage
+  pullUbuntuPackagingImage
+end
+
+function downloadStarter
+  runInContainer $UBUNTUBUILDIMAGE /scripts/downloadStarter.fish $argv
+end
+
+function downloadSyncer
+  runInContainer -e DOWNLOAD_SYNC_USER=$DOWNLOAD_SYNC_USER $UBUNTUBUILDIMAGE /scripts/downloadSyncer.fish $argv
+end
+
+function makeDockerImage
+  if test (count $argv) = 0
+    echo Must give image name as argument
+    return 1
+  end
+  set -l imagename $argv[1]
+
+  cd $WORKDIR/work/ArangoDB/build/install
+  and tar czvf $WORKDIR/arangodb.docker/install.tar.gz *
+  if test $status != 0
+    echo Could not create install tarball!
+    return 1
+  end
+
+  cd $WORKDIR/arangodb.docker
+  docker build -t $imagename .
+end
+
+function buildPackage
+  buildDebianPackage $argv
+  # buildRpmPackage $argv
+  # buildDockerImage $argv
+end
