122fcac837bb6e830009bfcf10600d94af6f5f03
Max Neunhoeffer <max@arangodb.com>
2018-02-19 23:23:22 +0100

    Another jenkins script to use $NODE_NAME subdirs of $HOME.

---
+++ jenkins/runPRtest2.fish
@@ -0,0 +1,18 @@
+#!/usr/bin/env fish
+mkdir -p $HOME/$NODE_NAME ; cd $HOME/$NODE_NAME
+if not cd oskar ^ /dev/null 
+  git clone https://github.com/neunhoef/oskar ; and cd oskar
+end
+and source helper.fish
+if test $status != 0 ; echo Did not find oskar and helpers ; exit 1 ; end
+
+lockDirectory ; updateOskar ; clearResults
+
+eval $EDITION ; eval $STORAGE_ENGINE ; eval $TEST_SUITE
+
+switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH
+and oskar1
+
+set -l s $status ; moveResultsToWorkspace ; unlockDirectory 
+exit $s
+
