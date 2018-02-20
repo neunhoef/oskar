60867808a11455ec20e3b02da48f720a63e4a505
Max Neunhoeffer <max@arangodb.com>
2018-02-20 23:19:23 +0100

    Add two more Docker image building scripts.

---
+++ jenkins/nightlyDocker3.2.fish
@@ -0,0 +1,36 @@
+#!/usr/bin/env fish
+mkdir -p "$HOME/x$NODE_NAME" ; cd "$HOME/x$NODE_NAME"
+if not cd oskar ^ /dev/null 
+  git clone https://github.com/neunhoef/oskar ; and cd oskar
+end
+and source helper.fish
+if test $status != 0 ; echo Did not find oskar and helpers ; exit 1 ; end
+
+lockDirectory ; updateOskar ; clearResults
+
+community
+
+switchBranches 3.2 3.2
+and findArangoDBVersion
+and buildStaticArangoDB
+and downloadStarter
+and makeDockerImage arangodb/arangodb-preview:3.2
+and docker push arangodb/arangodb-preview:3.2
+
+if test $status != 0
+  echo Production of community image failed, giving up...
+  exit 1
+end
+
+enterprise
+
+switchBranches 3.2 3.2
+and findArangoDBVersion
+and buildStaticArangoDB
+and downloadStarter
+and downloadSyncer
+and makeDockerImage registry.arangodb.biz:5000/arangodb/arangodb-preview:3.2-$KEY
+and docker push registry.arangodb.biz:5000/arangodb/arangodb-preview:3.2-$KEY
+
+set -l s $status ; unlockDirectory ; exit $s
+
---
+++ jenkins/nightlyDockerDevel.fish
@@ -0,0 +1,36 @@
+#!/usr/bin/env fish
+mkdir -p "$HOME/x$NODE_NAME" ; cd "$HOME/x$NODE_NAME"
+if not cd oskar ^ /dev/null 
+  git clone https://github.com/neunhoef/oskar ; and cd oskar
+end
+and source helper.fish
+if test $status != 0 ; echo Did not find oskar and helpers ; exit 1 ; end
+
+lockDirectory ; updateOskar ; clearResults
+
+community
+
+switchBranches devel devel
+and findArangoDBVersion
+and buildStaticArangoDB
+and downloadStarter
+and makeDockerImage arangodb/arangodb-preview:3.4.devel
+and docker push arangodb/arangodb-preview:3.4.devel.devel.devel.devel
+
+if test $status != 0
+  echo Production of community image failed, giving up...
+  exit 1
+end
+
+enterprise
+
+switchBranches devel devel
+and findArangoDBVersion
+and buildStaticArangoDB
+and downloadStarter
+and downloadSyncer
+and makeDockerImage registry.arangodb.biz:5000/arangodb/arangodb-preview:3.4.devel-$KEY
+and docker push registry.arangodb.biz:5000/arangodb/arangodb-preview:3.4.devel-$KEY
+
+set -l s $status ; unlockDirectory ; exit $s
+
