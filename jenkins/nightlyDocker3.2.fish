3aef5cb046afcf588bcc6bd45f9f16d2590cc278
Max Neunhoeffer <max@arangodb.com>
2018-03-02 13:23:09 +0100

    Push alpine Docker images to another location for 3.2.

--- jenkins/nightlyDocker3.2.fish
+++ jenkins/nightlyDocker3.2.fish
@@ -16,6 +16,8 @@ and buildStaticArangoDB
 and downloadStarter
 and makeDockerImage arangodb/arangodb-preview:3.2
 and docker push arangodb/arangodb-preview:3.2
+and docker tag arangodb/arangodb-preview:3.2 registry.arangodb.biz:5000/arangodb/linux-community-maintainer:3.2
+and docker push registry.arangodb.biz:5000/arangodb/linux-community-maintainer:3.2
 
 if test $status != 0
   echo Production of community image failed, giving up...
@@ -31,11 +33,15 @@ and downloadStarter
 and downloadSyncer
 and makeDockerImage registry.arangodb.biz:5000/arangodb/arangodb-preview:3.2-$KEY
 and docker push registry.arangodb.biz:5000/arangodb/arangodb-preview:3.2-$KEY
+and docker tag registry.arangodb.biz:5000/arangodb/arangodb-preview:3.2-$KEY registry.arangodb.biz:5000/arangodb/linux-enterprise-maintainer:3.2
+and docker push registry.arangodb.biz:5000/arangodb/linux-enterprise-maintainer:3.2
 
 and begin
   rm -rf $WORKSPACE/imagenames.log
   echo arangodb/arangodb-preview:3.2 >> $WORKSPACE/imagenames.log
+  echo registry.arangodb.biz:5000/arangodb/linux-community-maintainer:3.2 >> $WORKSPACE/imagenames.log
   echo registry.arangodb.biz:5000/arangodb/arangodb-preview:3.2-$KEY >> $WORKSPACE/imagenames.log
+  echo registry.arangodb.biz:5000/arangodb/linux-enterprise-maintainer:3.2 >> $WORKSPACE/imagenames.log
 end
 
 set -l s $status ; cd "$HOME/$NODE_NAME/oskar" ; unlockDirectory ; exit $s
