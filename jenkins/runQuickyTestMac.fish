25832e3cdf4a8c3f93fa5cfb10c12ca584164d9b
Max Neunhoeffer <max@arangodb.com>
2018-03-14 09:16:28 +0100

    Add Mac quicky test.

---
+++ jenkins/runQuickyTestMac.fish
@@ -0,0 +1,37 @@
+#!/usr/bin/env fish
+source jenkins/helper.jenkins.fish ; prepareOskar
+
+lockDirectory ; updateOskar ; clearResults
+
+echo Working on branch $ARANGODB_BRANCH of main repository and
+echo on branch $ENTERPRISE_BRANCH of enterprise repository.
+
+switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH
+or block
+  echo switchBranches error, giving up.
+  unlockDirectory
+  exit 1
+end
+
+parallelism 15
+
+enterprise ; rocksdb ; cluster
+
+oskar1
+or block
+  echo Errors in enterprise/rocksdb/cluster, stopping.
+  moveResultsToWorkspace
+  unlockDirectory
+  exit 1
+end
+  
+mv work/cmakeAlpine.log work/cmakeAlpine.log
+mv work/buildAlpine.log work/buildAlpineEnterprise.log
+
+community ; mmfiles ; single
+
+oskar1
+
+set -l s $status
+cd "$HOME/$NODE_NAME/oskar" ; moveResultsToWorkspace ; unlockDirectory 
+exit $s
