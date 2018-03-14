89f8e8b40d946c6cc8cb7d9ae59cb79369aed0d7
Max Neunhoeffer <max@arangodb.com>
2018-03-14 12:09:10 +0100

    New test script for a specific bug.

---
+++ jenkins/runClusterHangerLinux.fish
@@ -0,0 +1,22 @@
+#!/usr/bin/env fish
+source jenkins/helper.jenkins.fish ; prepareOskar
+
+lockDirectory ; updateOskar ; clearResults
+
+echo Working on branch $ARANGODB_BRANCH of main repository and
+echo on branch $ENTERPRISE_BRANCH of enterprise repository.
+
+switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH
+or begin
+  echo switchBranches error, giving up.
+  unlockDirectory
+  exit 1
+end
+
+enterprise ; mmfiles ; cluster
+
+oskar1
+
+set -l s $status
+cd "$HOME/$NODE_NAME/oskar" ; moveResultsToWorkspace ; unlockDirectory 
+exit $s
