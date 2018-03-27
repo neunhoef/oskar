69c73e8f6edaa45686d22dc5fa2df7b6728744a0
Sven Luschgy <sven@arangodb.com>
2018-03-27 07:32:48 +0200

    changed folder structure

---
+++ installer/mac/installer.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
+
+brew install ccache
+brew install fish
+brew install cmake
+brew install node
+brew install python@2
+brew install ruby
+
+gem install rspec
+gem install httparty
+gem install persistent_httparty
+
+brew upgrade
+brew link --overwrite python@2
