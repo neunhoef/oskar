df2d85c6b2d1aafa28ec9ff3299221aae5a64c2d
Max Neunhoeffer <max@arangodb.com>
2018-04-13 16:11:48 +0200

    Install openssl before openldap.

--- buildAlpine.docker/tools/install.sh
+++ buildAlpine.docker/tools/install.sh
@@ -4,6 +4,17 @@
 apk update
 apk add groff g++ bison flex make cmake ccache python libldap openssl-dev git linux-vanilla-dev linux-headers vim boost-dev ctags man gdb fish openssh db-dev libexecinfo-dev libexecinfo
 
+# Compile openssl1.1 library:
+wget https://www.openssl.org/source/openssl-1.1.0h.tar.gz
+tar xzvf openssl-1.1.0h.tar.gz
+cd openssl-1.1.0h
+./config
+make
+make test
+make install
+cd /tmp
+rm -rf openssl-1.1.0h.tar.gz openssl-1.1.0h
+
 # Compile openldap library:
 cd /tmp
 wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.45.tgz
@@ -15,17 +26,6 @@ make depend && make -j64
 make install
 cd /tmp
 rm -rf openldap-2.4.45.tgz openldap-2.4.45
-
-# Compile openssl1.1 library:
-wget https://www.openssl.org/source/openssl-1.1.0h.tar.gz
-tar xzvf openssl-1.1.0h.tar.gz
-cd openssl-1.1.0h
-./config
-make
-make test
-make install
-cd /tmp
-rm -rf openssl-1.1.0h.tar.gz openssl-1.1.0h
 
 # Make some warnings go away:
 echo "#include <poll.h>" > /usr/include/sys/poll.h
