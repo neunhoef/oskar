55d545f8c676326cb7066f70dfe6836be71831d5
Max Neunhoeffer <max@arangodb.com>
2018-04-13 16:11:10 +0200

    Install openssl-1.1.0h in alpine image.

--- buildAlpine.docker/tools/install.sh
+++ buildAlpine.docker/tools/install.sh
@@ -16,5 +16,16 @@ make install
 cd /tmp
 rm -rf openldap-2.4.45.tgz openldap-2.4.45
 
+# Compile openssl1.1 library:
+wget https://www.openssl.org/source/openssl-1.1.0h.tar.gz
+tar xzvf openssl-1.1.0h.tar.gz
+cd openssl-1.1.0h
+./config
+make
+make test
+make install
+cd /tmp
+rm -rf openssl-1.1.0h.tar.gz openssl-1.1.0h
+
 # Make some warnings go away:
 echo "#include <poll.h>" > /usr/include/sys/poll.h
