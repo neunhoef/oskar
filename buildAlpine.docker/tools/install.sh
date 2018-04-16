dd020c95377d03336219defe87e7acf2d0389ed7
Max Neunhoeffer <max@arangodb.com>
2018-04-16 10:05:38 +0200

    Move to openldap 2.4.46.

--- buildAlpine.docker/tools/install.sh
+++ buildAlpine.docker/tools/install.sh
@@ -5,16 +5,17 @@ apk update
 apk add groff g++ bison flex make cmake ccache python libldap openssl-dev git linux-vanilla-dev linux-headers vim boost-dev ctags man gdb fish openssh db-dev libexecinfo-dev libexecinfo
 
 # Compile openldap library:
+export OPENLDAP_VERSION=2.4.46
 cd /tmp
-wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.45.tgz
-tar xzvf openldap-2.4.45.tgz
-cd openldap-2.4.45
+wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-$OPENLDAP_VERSION.tgz
+tar xzvf openldap-$OPENLDAP_VERSION.tgz
+cd openldap-$OPENLDAP_VERSION
 cp -a /tools/config.* ./build
 ./configure --prefix=/usr --enable-static
 make depend && make -j64
 make install
 cd /tmp
-rm -rf openldap-2.4.45.tgz openldap-2.4.45
+rm -rf openldap-$OPENLDAP_VERSION.tgz openldap-$OPENLDAP_VERSION
 
 # Make some warnings go away:
 echo "#include <poll.h>" > /usr/include/sys/poll.h
