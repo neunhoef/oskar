cab6b33f312adcfc27746d38171b5ebbc79ccaad
Max Neunhoeffer <max@arangodb.com>
2018-04-16 09:32:17 +0200

    Go to openldap 2.4.46

--- buildAlpine.docker/tools/install.sh
+++ buildAlpine.docker/tools/install.sh
@@ -10,23 +10,22 @@ wget https://www.openssl.org/source/openssl-1.1.0h.tar.gz
 tar xzvf openssl-1.1.0h.tar.gz
 cd openssl-1.1.0h
 ./config
-make
-make test
-make install
+make build_libs
+make install_dev
 cd /tmp
 rm -rf openssl-1.1.0h.tar.gz openssl-1.1.0h
 
 # Compile openldap library:
 cd /tmp
-wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.45.tgz
-tar xzvf openldap-2.4.45.tgz
-cd openldap-2.4.45
+wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.46.tgz
+tar xzvf openldap-2.4.46.tgz
+cd openldap-2.4.46
 cp -a /tools/config.* ./build
 ./configure --prefix=/usr --enable-static
 make depend && make -j64
 make install
 cd /tmp
-rm -rf openldap-2.4.45.tgz openldap-2.4.45
+rm -rf openldap-2.4.46.tgz openldap-2.4.46
 
 # Make some warnings go away:
 echo "#include <poll.h>" > /usr/include/sys/poll.h
