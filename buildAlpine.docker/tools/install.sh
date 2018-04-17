821e7435411cff4cdd61f28501c30c97d72a27ec
Max Neunhoeffer <max@arangodb.com>
2018-04-17 22:25:09 +0200

    Make versions configurable.

--- buildAlpine.docker/tools/install.sh
+++ buildAlpine.docker/tools/install.sh
@@ -5,27 +5,29 @@ apk update
 apk add groff g++ bison flex make cmake ccache python libldap git linux-vanilla-dev linux-headers vim boost-dev ctags man gdb fish openssh db-dev libexecinfo-dev libexecinfo file libltdl zlib-dev
 
 # Compile openssl1.1 library:
+export OPENSSLVERSION=1.1.0h
 cd /tmp
-wget https://www.openssl.org/source/openssl-1.1.0h.tar.gz
-tar xzvf openssl-1.1.0h.tar.gz
-cd openssl-1.1.0h
+wget https://www.openssl.org/source/openssl-$OPENSSLVERSION.tar.gz
+tar xzvf openssl-$OPENSSLVERSION.tar.gz
+cd openssl-$OPENSSLVERSION
 ./config --prefix=/usr no-async
 make build_libs
 make install_dev
 cd /tmp
-rm -rf openssl-1.1.0h.tar.gz openssl-1.1.0h
+rm -rf openssl-$OPENSSLVERSION.tar.gz openssl-$OPENSSLVERSION
 
 # Compile openldap library:
+export OPENLDAPVERSION=2.4.46
 cd /tmp
-wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-2.4.46.tgz
-tar xzvf openldap-2.4.46.tgz
-cd openldap-2.4.46
+wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-$OPENLDAPVERSION.tgz
+tar xzvf openldap-$OPENLDAPVERSION.tgz
+cd openldap-$OPENLDAPVERSION
 cp -a /tools/config.* ./build
 ./configure --prefix=/usr --enable-static
 make depend && make -j64
 make install
 cd /tmp
-rm -rf openldap-2.4.46.tgz openldap-2.4.46
+rm -rf openldap-$OPENLDAPVERSION.tgz openldap-$OPENLDAPVERSION
 
 # Make some warnings go away:
 echo "#include <poll.h>" > /usr/include/sys/poll.h
