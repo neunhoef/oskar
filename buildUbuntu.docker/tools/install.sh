8f6ef0cc420596c7a753a49e4dc1168c6261f19f
Max Neunhoeffer <max@arangodb.com>
2018-04-17 22:27:50 +0200

    Move to openssl 1.1.

--- buildUbuntu.docker/tools/install.sh
+++ buildUbuntu.docker/tools/install.sh
@@ -1,9 +1,5 @@
 #!/bin/sh
 
-# Install some packages:
-apk update
-apk add groff g++ bison flex make cmake ccache python libldap git linux-vanilla-dev linux-headers vim boost-dev ctags man gdb fish openssh db-dev libexecinfo-dev libexecinfo file libltdl zlib-dev
-
 # Compile openssl1.1 library:
 export OPENSSLVERSION=1.1.0h
 cd /tmp
@@ -11,8 +7,9 @@ wget https://www.openssl.org/source/openssl-$OPENSSLVERSION.tar.gz
 tar xzvf openssl-$OPENSSLVERSION.tar.gz
 cd openssl-$OPENSSLVERSION
 ./config --prefix=/usr no-async
-make build_libs
-make install_dev
+make
+make test
+make install
 cd /tmp
 rm -rf openssl-$OPENSSLVERSION.tar.gz openssl-$OPENSSLVERSION
 
@@ -28,6 +25,3 @@ make depend && make -j64
 make install
 cd /tmp
 rm -rf openldap-$OPENLDAPVERSION.tgz openldap-$OPENLDAPVERSION
-
-# Make some warnings go away:
-echo "#include <poll.h>" > /usr/include/sys/poll.h
