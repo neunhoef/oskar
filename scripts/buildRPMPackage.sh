24cb51906adfbe2cc1a739541a81079f7987c334
Max Neunhoeffer <max@arangodb.com>
2018-04-19 00:06:23 +0200

    User rights and copy rpm packages out.

--- scripts/buildRPMPackage.sh
+++ scripts/buildRPMPackage.sh
@@ -1,12 +1,10 @@
 #!/bin/sh
 mkdir -p /root/SPECS
-mkdir -p /root/RPMS
-mkdir -p /root/SRPMS
-mkdir -p /root/SOURCES
-mkdir -p /root/BUILD
 cd $INNERWORKDIR
 cp arangodb3.spec /root/SPECS
 cp arangodb3.initd arangodb3.service arangodb3.logrotate $INNERWORKDIR/ArangoDB/build/install/usr/share/arangodb3
 rpmbuild -bb /root/SPECS/arangodb3.spec
+cp /root/rpmbuild/RPMS/*/*.rpm $INNERWORKDIR
+chown -R $UID.$GID $INNERWORKDIR
 echo Sleeping
-sleep 60
+sleep 180
