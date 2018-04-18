f478d0b088255de87a6800841cfb95e29ad4a3d7
Max Neunhoeffer <max@arangodb.com>
2018-04-18 23:56:21 +0200

    Fix logrotate stuff.

--- scripts/buildRPMPackage.sh
+++ scripts/buildRPMPackage.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
-mkdir -p /root/{SPECS,RPMS,SRPMS,SOURCES,BUILD}
+mkdir -p /root/SPECS
+mkdir -p /root/RPMS
+mkdir -p /root/SRPMS
+mkdir -p /root/SOURCES
+mkdir -p /root/BUILD
 cd $INNERWORKDIR
 cp arangodb3.spec /root/SPECS
 cp arangodb3.initd arangodb3.service arangodb3.logrotate $INNERWORKDIR/ArangoDB/build/install/usr/share/arangodb3
