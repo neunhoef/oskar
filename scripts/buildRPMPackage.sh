b5e3be9ad38473558d7880ff599448309afc9a13
Max Neunhoeffer <max@arangodb.com>
2018-04-19 09:32:45 +0200

    Error handling.

--- scripts/buildRPMPackage.sh
+++ scripts/buildRPMPackage.sh
@@ -3,8 +3,8 @@ mkdir -p /root/SPECS
 cd $INNERWORKDIR
 cp arangodb3.spec /root/SPECS
 cp arangodb3.initd arangodb3.service arangodb3.logrotate $INNERWORKDIR/ArangoDB/build/install/usr/share/arangodb3
-rpmbuild -bb /root/SPECS/arangodb3.spec
-cp /root/rpmbuild/RPMS/*/*.rpm $INNERWORKDIR
+rpmbuild -bb /root/SPECS/arangodb3.spec ; or exit 1
+cp /root/rpmbuild/RPMS/*/*.rpm $INNERWORKDIR ; or exit 1
 chown -R $UID.$GID $INNERWORKDIR
 echo Sleeping
-sleep 180
+sleep 60
