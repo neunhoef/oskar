785c57524ba1baafb81705d466e2180c977a56a3
Max Neunhoeffer <max@arangodb.com>
2018-04-23 22:47:18 +0200

    Use curl instead of wget for downloads.

--- buildAlpine.docker/tools/install.sh
+++ buildAlpine.docker/tools/install.sh
@@ -7,7 +7,7 @@ apk add groff g++ bison flex make cmake ccache python libldap git linux-vanilla-
 # Compile openssl1.1 library:
 export OPENSSLVERSION=1.1.0h
 cd /tmp
-wget https://www.openssl.org/source/openssl-$OPENSSLVERSION.tar.gz
+curl -O https://www.openssl.org/source/openssl-$OPENSSLVERSION.tar.gz
 tar xzvf openssl-$OPENSSLVERSION.tar.gz
 cd openssl-$OPENSSLVERSION
 ./config --prefix=/usr no-async
@@ -19,7 +19,7 @@ rm -rf openssl-$OPENSSLVERSION.tar.gz openssl-$OPENSSLVERSION
 # Compile openldap library:
 export OPENLDAPVERSION=2.4.46
 cd /tmp
-wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-$OPENLDAPVERSION.tgz
+curl -O ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-$OPENLDAPVERSION.tgz
 tar xzvf openldap-$OPENLDAPVERSION.tgz
 cd openldap-$OPENLDAPVERSION
 cp -a /tools/config.* ./build
--- buildUbuntu.docker/tools/install.sh
+++ buildUbuntu.docker/tools/install.sh
@@ -3,7 +3,7 @@
 # Compile openssl1.1 library:
 export OPENSSLVERSION=1.1.0h
 cd /tmp
-wget https://www.openssl.org/source/openssl-$OPENSSLVERSION.tar.gz
+curl -O https://www.openssl.org/source/openssl-$OPENSSLVERSION.tar.gz
 tar xzvf openssl-$OPENSSLVERSION.tar.gz
 cd openssl-$OPENSSLVERSION
 ./config --prefix=/usr no-async
@@ -16,7 +16,7 @@ rm -rf openssl-$OPENSSLVERSION.tar.gz openssl-$OPENSSLVERSION
 # Compile openldap library:
 export OPENLDAPVERSION=2.4.46
 cd /tmp
-wget ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-$OPENLDAPVERSION.tgz
+curl -O ftp://ftp.openldap.org/pub/OpenLDAP/openldap-release/openldap-$OPENLDAPVERSION.tgz
 tar xzvf openldap-$OPENLDAPVERSION.tgz
 cd openldap-$OPENLDAPVERSION
 cp -a /tools/config.* ./build
