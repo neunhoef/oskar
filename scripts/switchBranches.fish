256f59eb0c0ddd99a3c1d7e2bb1e320bb510635b
Max Neunhoeffer <max@arangodb.com>
2018-05-03 11:13:50 +0200

    Use git fetch.

--- scripts/switchBranches.fish
+++ scripts/switchBranches.fish
@@ -1,13 +1,13 @@
 #!/usr/bin/env fish
 cd $INNERWORKDIR/ArangoDB
 and git checkout -- .
-and git pull
+and git fetch
 and git checkout $argv[1]
 and git pull
 and if test $ENTERPRISEEDITION = On
   cd enterprise
   git checkout -- .
-  and git pull
+  and git fetch
   and git checkout $argv[2]
   and git pull
 end
