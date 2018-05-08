756d0693010412b3749bf8b7c874e40c393e756d
Michael Hackstein <michael@arangodb.com>
2018-05-08 10:30:24 +0200

    Increased Parallelism on Mac to 6. Seems to be a little bit better on the Trade-off

--- jenkins/runPRtestMac.fish
+++ jenkins/runPRtestMac.fish
@@ -4,7 +4,7 @@ source jenkins/helper.jenkins.fish ; prepareOskar
 lockDirectory ; updateOskar ; clearResults
 
 eval $EDITION ; eval $STORAGE_ENGINE ; eval $TEST_SUITE
-parallelism 4
+parallelism 6
 
 switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH
 and oskar1
