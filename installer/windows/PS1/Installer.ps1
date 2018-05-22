982e20c888b6c558caf23a3b9be00c1e4978afa2
Sven Luschgy <sven@arangodb.com>
2018-05-22 14:04:07 +0200

    removed MSVC via Choco, added CLCHEAT, removed OPENSSLDIR Env

--- installer/windows/PS1/Installer.ps1
+++ installer/windows/PS1/Installer.ps1
@@ -86,11 +86,16 @@ $arguments = @'
 '@
 ExternalProcess -process Powershell -arguments $arguments -wait $true
 
-$arguments = @("choco install -y dotnet4.7.1 vcredist-all cmake.portable nsis python2 procdump windbg wget nuget.commandline vim notepadplusplus putty.install openssh","choco install -y git winflexbison3 ruby","choco install -y ruby2.devkit nodejs jdk8",'choco install -y visualstudio2017enterprise --package-parameters "--add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.NativeCrossPlat --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended --includeOptional --passive"',"gem install bundler persistent_httparty rspec rspec-core","npm install -g gitbook-cli","pip3.5 install git+https://github.com/frerich/clcache.git")
+$arguments = @("choco install -y dotnet4.7.1 vcredist-all cmake.portable nsis python2 procdump windbg wget nuget.commandline vim notepadplusplus putty.install openssh","choco install -y git winflexbison3 ruby","choco install -y ruby2.devkit nodejs jdk8","gem install bundler persistent_httparty rspec rspec-core","npm install -g gitbook-cli","pip3.5 install git+https://github.com/frerich/clcache.git")
 ForEach($argument in $arguments)
 {
     ExternalProcess -process Powershell -arguments $argument -wait $true
 }
+
+DownloadFile -src 'https://aka.ms/vs/15/release/vs_community.exe' -dest "C:\Windows\Temp\vs_community.exe"
+$arguments = "--add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.NativeCrossPlat --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended --includeOptional --quiet"
+ExternalProcess -process "C:\Windows\Temp\vs_community.exe" -arguments $arguments -wait $true
+Remove-Item "C:\Windows\Temp\vs_community.exe"
 
 DownloadFile -src 'https://www.npcglib.org/~stathis/downloads/openssl-1.1.0f-vs2017.7z' -dest "C:\Windows\Temp\openssl-1.1.0f-vs2017.7z"
 ExternalProcess -process 7za -arguments "x C:\Windows\Temp\openssl-1.1.0f-vs2017.7z -oC:\" -wait $true
@@ -98,12 +103,12 @@ Rename-Item "C:\openssl-1.1.0f-vs2017" "C:\OpenSSL-Win64"
 [Environment]::SetEnvironmentVariable("OPENSSLDIR", "C:\OpenSSL-Win64", "Machine")
 Remove-Item "C:\Windows\Temp\openssl-1.1.0f-vs2017.7z"
 
-DownloadFile -src 'https://github.com/frerich/clcache/releases/download/v4.1.0/clcache-4.1.0.zip' -dest "C:\Windows\Temp\clcache-4.1.0.zip"
 $clpath = $(Split-Path -Parent $(Get-ChildItem $(Get-VSSetupInstance).InstallationPath -Filter cl.exe -Recurse | Select-Object Fullname |Where {$_.FullName -match "Hostx64\\x64"}).FullName) 
+DownloadFile -src 'https://github.com/frerich/clcache/releases/download/v4.1.0/clcache-4.1.0.zip' -dest "C:\Windows\Temp\clcache-4.1.0.zip"
+DownloadFile -src 'https://github.com/arangodb-helper/clcheat/raw/master/clcheat.exe' -dest $clpath
 Expand-Archive -Force "C:\Windows\Temp\clcache-4.1.0.zip" "$clpath"
 Rename-Item -Path "$clpath\cl.exe" -NewName "clo.exe"
 Rename-Item -Path "$clpath\cl.exe.config" -NewName "clo.exe.config"
-Rename-Item -Path "$clpath\clcache.exe" -NewName "cl.exe"
-Rename-Item -Path "$clpath\clcache.exe.manifest" -NewName "cl.exe.manifest"
+Rename-Item -Path "$clpath\clcheat.exe" -NewName "cl.exe"
 [Environment]::SetEnvironmentVariable("CLCACHE_CL", "$($(Get-ChildItem $(Get-VSSetupInstance).InstallationPath -Filter clo.exe -Recurse | Select-Object Fullname |Where {$_.FullName -match "Hostx64\\x64"}).FullName)", "Machine")
 Remove-Item "C:\Windows\Temp\clcache-4.1.0.zip"
