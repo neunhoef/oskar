0308da1c7d22609c8c28567ec926a840725497d9
Jan Christoph Uhde <ObiWahn@users.noreply.github.com>
2018-05-24 13:53:41 +0200

    allow oskar to switch branches in a more robust way (#3)
    This allows for checking out branches even if the remote
    repository was rewritten (git push --force).

--- scripts/switchBranches.fish
+++ scripts/switchBranches.fish
@@ -3,11 +3,11 @@ cd $INNERWORKDIR/ArangoDB
 and git checkout -- .
 and git fetch
 and git checkout $argv[1]
-and git pull
+and git reset --hard origin/$argv[1]
 and if test $ENTERPRISEEDITION = On
   cd enterprise
   git checkout -- .
   and git fetch
   and git checkout $argv[2]
-  and git pull
+  and git reset --hard origin/$argv[2]
 end
