bedbd189136e3a4e1f8cf97a4aeabd457e03386c
Sven Luschgy <sven@arangodb.com>
2018-05-29 15:00:03 +0200

    fixed a path problem

--- installer/windows/PS1/Installer.ps1
+++ installer/windows/PS1/Installer.ps1
@@ -93,19 +93,19 @@ ForEach($argument in $arguments)
 }
 
 DownloadFile -src 'https://aka.ms/vs/15/release/vs_community.exe' -dest "C:\Windows\Temp\vs_community.exe"
-$arguments = "--add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.NativeCrossPlat --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended --includeOptional --quiet"
+$arguments = "--add Microsoft.VisualStudio.Workload.Node --add Microsoft.VisualStudio.Workload.NativeCrossPlat --add Microsoft.VisualStudio.Workload.NativeDesktop --includeRecommended --includeOptional -p"
 ExternalProcess -process "C:\Windows\Temp\vs_community.exe" -arguments $arguments -wait $true
 Remove-Item "C:\Windows\Temp\vs_community.exe"
 
 DownloadFile -src 'https://www.npcglib.org/~stathis/downloads/openssl-1.1.0f-vs2017.7z' -dest "C:\Windows\Temp\openssl-1.1.0f-vs2017.7z"
 ExternalProcess -process 7za -arguments "x C:\Windows\Temp\openssl-1.1.0f-vs2017.7z -oC:\" -wait $true
 Rename-Item "C:\openssl-1.1.0f-vs2017" "C:\OpenSSL-Win64"
-#[Environment]::SetEnvironmentVariable("OPENSSLDIR", "C:\OpenSSL-Win64", "Machine")
+[Environment]::SetEnvironmentVariable("OPENSSLDIR", "C:\OpenSSL-Win64", "Machine")
 Remove-Item "C:\Windows\Temp\openssl-1.1.0f-vs2017.7z"
 
 $clpath = $(Split-Path -Parent $(Get-ChildItem $(Get-VSSetupInstance).InstallationPath -Filter cl.exe -Recurse | Select-Object Fullname |Where {$_.FullName -match "Hostx64\\x64"}).FullName) 
 DownloadFile -src 'https://github.com/frerich/clcache/releases/download/v4.1.0/clcache-4.1.0.zip' -dest "C:\Windows\Temp\clcache-4.1.0.zip"
-DownloadFile -src 'https://github.com/arangodb-helper/clcheat/raw/master/clcheat.exe' -dest $clpath
+DownloadFile -src 'https://github.com/arangodb-helper/clcheat/raw/master/clcheat.exe' -dest "$clpath\clcheat.exe"
 Expand-Archive -Force "C:\Windows\Temp\clcache-4.1.0.zip" "$clpath"
 Rename-Item -Path "$clpath\cl.exe" -NewName "clo.exe"
 Rename-Item -Path "$clpath\cl.exe.config" -NewName "clo.exe.config"
