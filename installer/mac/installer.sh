e484f0b035c96e2375b4fb3c252918eb6f603954
Sven Luschgy <sven@arangodb.com>
2018-06-18 15:09:39 +0200

    added java to mac installer & fixed filemode

--- installer/mac/installer.sh
+++ installer/mac/installer.sh
@@ -2,6 +2,9 @@
 
 /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 
+brew update
+brew tap caskroom/cask
+
 brew install ccache
 brew install fish
 brew install cmake
@@ -10,6 +13,8 @@ brew install python@2
 brew install ruby
 brew install jq
 brew install git
+
+brew install Caskroom/cask/java
 
 gem install rspec
 gem install httparty
