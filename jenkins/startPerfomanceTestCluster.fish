8e6d6782d8927a92adff32876b017a504e8bc61e
Michael Hackstein <michael@arangodb.com>
2018-07-18 13:30:01 +0200

    Use starter start to deploy in Background

--- jenkins/startPerfomanceTestCluster.fish
+++ jenkins/startPerfomanceTestCluster.fish
@@ -7,19 +7,20 @@ end
 set -g JOIN_PART "--join $argv[1] --join $argv[2] --join $argv[3]"
 
 function startClusterStarter
-  set -l DATA_PATH "$INNERWORKDIR/perfCluster"
+  set -l LOCALWORKDIR "$WORKDIR/$INNERWORKDIR"
+  set -l DATA_PATH "$LOCALWORKD/perfCluster"
   rm -rf DATA_PATH
   if test "$ENTERPRISEEDITION" = "On"
-    set -l ENTERPRISE_JS_PATH "--all.javascript.module-path $INNERWORKDIR/ArangoDB/enterprise/js"
+    set -l ENTERPRISE_JS_PATH "--all.javascript.module-path $LOCALWORKDIR/ArangoDB/enterprise/js"
   else
     set -l ENTERPRISE_JS_PATH ""
   end
-  set -l JS_PATH "$INNERWORKDIR/ArangoDB/js"
-  set -l ARANGOD_PATH "$INNERWORKDIR/ArangoDB/build/bin/arangod"
-  set -l STARTER "$INNERWORKDIR/ArangoDB/build/install/usr/bin/arangodb"
+  set -l JS_PATH "$LOCALWORKDIR/ArangoDB/js"
+  set -l ARANGOD_PATH "$LOCALWORKDIR/ArangoDB/build/bin/arangod"
+  set -l STARTER "$LOCALWORKDIR/ArangoDB/build/install/usr/bin/arangodb"
   # Tell jenkins to not kill this job.
   set -xg BUILD_ID=dontKillMe
-  eval (runInContainer $STARTER --starter.data-dir $DATA_PATH --server.js-dir $JS_PATH --server.arangod $ARANGOD_PATH $ENTERPRISE_JS_PATH $JOIN_PART) &
+  eval ($STARTER start --starter.wait --starter.data-dir $DATA_PATH --server.js-dir $JS_PATH --server.arangod $ARANGOD_PATH $ENTERPRISE_JS_PATH $JOIN_PART)
 end
 
 source jenkins/helper.jenkins.fish ; prepareOskar
