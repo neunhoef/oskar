0e61d2f5e21775b4d0c81034d6ebcb03b1dd9275
Michael Hackstein <michael@arangodb.com>
2018-07-20 11:42:06 +0200

    Try to use github proxy to update oskar as well

--- jenkins/helper.jenkins.fish
+++ jenkins/helper.jenkins.fish
@@ -3,7 +3,7 @@
 function prepareOskar
   mkdir -p "$HOME/$NODE_NAME" ; cd "$HOME/$NODE_NAME"
   if not cd oskar ^ /dev/null 
-    git clone https://github.com/neunhoef/oskar ; and cd oskar
+    git clone https://github-proxy01:8088/neunhoef/oskar ; and cd oskar
   end
   and source helper.fish
   if test $status != 0 ; echo Did not find oskar and helpers ; exit 1 ; end
