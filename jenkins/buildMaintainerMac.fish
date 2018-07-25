e9140187ad55ee4878e2fd28504361be96e727bc
Frank Celler <frank@arangodb.com>
2018-07-25 12:41:22 +0200

    reduce concurrency on MAC

---
+++ jenkins/buildMaintainerMac.fish
@@ -0,0 +1,22 @@
+#!/usr/bin/env fish
+source jenkins/helper.jenkins.fish ; prepareOskar
+
+lockDirectory ; updateOskar ; clearResults
+rocksdb ; cluster ; maintainerOn
+
+eval $EDITION
+parallelism 8
+
+echo Working on branch $ARANGODB_BRANCH of main repository and
+echo on branch $ENTERPRISE_BRANCH of enterprise repository.
+
+switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH
+and buildStaticArangoDB
+
+set -l s $status
+if test $s != 0
+  echo Build failure with maintainer mode on in community.
+end
+cd "$HOME/$NODE_NAME/oskar" ; moveResultsToWorkspace ; unlockDirectory 
+exit $s
+
