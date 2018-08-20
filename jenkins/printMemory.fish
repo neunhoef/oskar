e31d3b7f6a98d30059dfa42d3e85296bc8daee2a
Michael Hackstein <michael@arangodb.com>
2018-08-20 15:05:33 +0200

    Fixed variable usage

--- jenkins/printMemory.fish
+++ jenkins/printMemory.fish
@@ -2,15 +2,15 @@
 
 set -l LOCALWORKDIR $argv[1]
 set -l DBOUT "$LOCALWORKDIR/dbserver.csv"
-rm -rf DBOUT
+rm -rf $DBOUT
 set -l COUT "$LOCALWORKDIR/coordinator.csv"
-rm -rf COUT
+rm -rf $COUT
 set -l pPid (ps ax | grep arango | grep PRIMARY | head -1 | awk '{print $1}')
 set -l cPid (ps ax | grep arango | grep COORDINATOR | head -1 | awk '{print $1}')
 
 while true
-  ps -p $pPid -o "%cpu,%mem" --no-header >> DBOUT
-  ps -p $cPid -o "%cpu,%mem" --no-header >> COUT
+  ps -p $pPid -o "%cpu,%mem" --no-header >> $DBOUT
+  ps -p $cPid -o "%cpu,%mem" --no-header >> $COUT
   sleep 5
 end
 
