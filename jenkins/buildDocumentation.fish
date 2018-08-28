04b555dacd3f2548d30b99169053c84673036eb1
Jan Christoph Uhde <Jan@UhdeJC.com>
2018-08-28 16:54:23 +0200

    update documentation job

--- jenkins/buildDocumentation.fish
+++ jenkins/buildDocumentation.fish
@@ -1,21 +1,22 @@
 #!/usr/bin/env fish
-source jenkins/helper.jenkins.fish ; prepareOskar
-
-lockDirectory ; updateOskar ; clearResults
-rocksdb ; cluster ; maintainerOff ; community
+#source jenkins/helper.jenkins.fish
+source helper.fish
+and rocksdb ; cluster ; maintainerOff ; community
+#and prepareOskar; lockDirectory ; updateOskar ; clearResults
 
 echo "--------------------------------------------------------------------------------"
 showConfig
 
-echo Working on branch $ARANGODB_BRANCH of main repository and
-echo on branch $ENTERPRISE_BRANCH of enterprise repository.
-
-switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH
-and buildStaticArangoDB and buildDocumentationInPr
+echo Working on branch $ARANGODB_BRANCH of main repository
+and echo on branch $ENTERPRISE_BRANCH of enterprise repository.
+and switchBranches $ARANGODB_BRANCH $ENTERPRISE_BRANCH
+and buildStaticArangoDB
+and buildDocumentationInPr
 
 set -l s $status
 if test $s != 0
   echo Build failure with maintainer mode off in $EDITION.
 end
-cd "$HOME/$NODE_NAME/oskar" ; moveResultsToWorkspace ; unlockDirectory 
+
+cd "$HOME/$NODE_NAME/oskar" ; moveResultsToWorkspace ; unlockDirectory
 exit $s
