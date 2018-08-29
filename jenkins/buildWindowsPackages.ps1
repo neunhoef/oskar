aaa9b11bf0836cd7fa7d327739d0427b00e95b4f
Sven Luschgy <sven@arangodb.com>
2018-08-29 21:21:01 +0200

    added Windows Script

---
+++ jenkins/buildWindowsPackages.ps1
@@ -0,0 +1,53 @@
+$HDD = $(Split-Path -Qualifier $ENV:WORKSPACE)
+If(-Not(Test-Path -PathType Container -Path "$HDD\$env:NODE_NAME"))
+{
+    New-Item -ItemType Directory -Path "$HDD\$env:NODE_NAME"
+}
+$OSKARDIR = "$HDD\$env:NODE_NAME"
+Set-Location $OSKARDIR
+If(-Not(Test-Path -PathType Container -Path "$OSKARDIR\oskar"))
+{
+    git clone https://github.com/neunhoef/oskar
+    Set-Location "$OSKARDIR\oskar"
+}
+Else
+{
+    Set-Location "$OSKARDIR\oskar"
+    git pull
+}
+Import-Module "$OSKARDIR\oskar\helper.psm1"
+If(-Not($?))
+{
+    Write-Host "Did not find oskar modul"
+    Exit 1
+}
+
+lockDirectory
+updateOskar
+If($(Get-Module).Name -ccontains "oskar")
+{
+    Remove-Module oskar
+}
+Import-Module "$OSKARDIR\oskar\helper.psm1"
+clearResults
+
+. $env:STATICEXECUTABLES
+. $env:MAINTAINER
+. $env:BUILDMODE
+
+switchBranches $env:ARANGODB_BRANCH $env:ENTERPRISE_BRANCH
+If ($global:ok) 
+{
+    makeRelease
+}
+$s = $global:ok
+unlockDirectory
+
+If($s)
+{
+    Exit 0
+}
+Else
+{
+    Exit 1
+} 
