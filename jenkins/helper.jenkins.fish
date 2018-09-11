39f833cb6312b68a2e60ca8f74685975ef03ee2c
Frank Celler <frank@arangodb.com>
2018-09-11 15:43:50 +0200

    clone into correct directory

--- jenkins/helper.jenkins.fish
+++ jenkins/helper.jenkins.fish
@@ -5,7 +5,7 @@ function prepareOskar
 
   mkdir -p "$HOME/$NODE_NAME" ; cd "$HOME/$NODE_NAME"
   if not cd $OSKAR ^ /dev/null 
-    git clone https://github.com/arangodb/oskar ; and cd $OSKAR
+    git clone https://github.com/arangodb/oskar $OSKAR ; and cd $OSKAR
   else
     git fetch ; and git reset --hard origin/master
   end
