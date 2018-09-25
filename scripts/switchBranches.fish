3fba8a0542095172414dc2b82a2b611d43bbe586
Michael Hackstein <michael@arangodb.com>
2018-09-25 15:04:26 +0200

    Fixed all other parts of the switch branches script. Testing locally helps soooo much, do not understand why others don't bother...

--- scripts/switchBranches.fish
+++ scripts/switchBranches.fish
@@ -1,5 +1,25 @@
 #!/usr/bin/env fish
 
+function checkoutRepo
+  if test (count $argv) -ne 2
+      echo "Checkout needs two parameters branch force"
+      return 1
+  end
+  set -l branch $argv[1]
+  set -l clean $argv[2]
+
+  git checkout -- .
+  and git fetch
+  and git checkout $branch
+  and if test "$clean" = "true"
+    git reset --hard origin/$branch
+    and git clean -fdx
+  else
+    git pull
+  end
+  return $status
+end
+
 if test (count $argv) -lt 2
     echo "you did not provide enough arguments"
 end
@@ -9,31 +29,21 @@ set -l enterprise $argv[2]
 set -l force_clean false
 
 if test (count $argv) -eq 3
-    set -l force_clean $argv[3]
+    set force_clean $argv[3]
 end
 
-if test "$force_clean" = "true"
-  cd $INNERWORKDIR/ArangoDB
-  and git checkout -- .
-  and git fetch
-  and git checkout $arango
-  and git reset --hard origin/$arango
-  and git clean -fdx
-  and if test $ENTERPRISEEDITION = On
-    cd enterprise
-    and git checkout -- .
-    and git fetch
-    and git checkout $enterprise
-    and git reset --hard origin/$enterprise
-    and git clean -fdx
-  end
-else
-  cd $INNERWORKDIR/ArangoDB
-  and git checkout $arango
-  and git pull
-  and if test $ENTERPRISEEDITION = On
-    cd enterprise
-    and git checkout $enterprise
-    and git pull
+cd $INNERWORKDIR/ArangoDB
+and checkoutRepo $arango $force_clean
+if test $status -ne 0
+  echo "Failed to checkout community branch"
+  exit $status
+end
+
+if test $ENTERPRISEEDITION = On
+  cd enterprise
+  and checkoutRepo $enterprise $force_clean
+  if test $status -ne 0
+    echo "Failed to checkout enterprise branch"
+    exit $status
   end
 end
