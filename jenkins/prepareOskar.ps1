395e0d6563f0665472a0b479fb8ec309bee45f06
Luschgy <34065926+Luschgy@users.noreply.github.com>
2018-10-16 13:56:15 +0200

    Feature/windows get lockingprocess (#23)
    * added Get-LockingProcess Function & Stop-Process loop
    
    * added procdump
    
    * removed Get-LockedFile function and changed aproach

--- jenkins/prepareOskar.ps1
+++ jenkins/prepareOskar.ps1
@@ -1,14 +1,32 @@
-﻿If(-Not($env:OSKAR_BRANCH))
-{
-    $env:OSKAR_BRANCH = "master"
-}
-$HDD = $(Split-Path -Qualifier $env:WORKSPACE)
+﻿$HDD = $(Split-Path -Qualifier $env:WORKSPACE)
 If(-Not(Test-Path -PathType Container -Path "$HDD\$env:NODE_NAME"))
 {
     New-Item -ItemType Directory -Path "$HDD\$env:NODE_NAME"
 }
+
 $OSKARDIR = "$HDD\$env:NODE_NAME"
 Set-Location $OSKARDIR
+
+If(-Not(Test-Path -PathType Container -Path "$HDD\procdump"))
+{
+    New-Item -ItemType Directory -Path "$HDD\procdump"
+}
+
+$REGEX = [Regex]::new("pid: \d+")
+ForEach($LINE in (&(Get-Command handle64) $OSKARDIR))
+{
+    $VALUE = $REGEX.Match($LINE).Value
+    $ID = $VALUE.Split(' ',[System.StringSplitOptions]::RemoveEmptyEntries) | select -Last 1
+    Write-Host "$((Get-Command procdump).Source) -accepteula -ma $ID $HDD\procdump\$ID.dmp"
+    Start-Process $(Get-Command procdump) -ArgumentList "-accepteula -ma $ID $HDD\procdump\$ID.dmp"
+    Write-Host "Stop-Process -Force -Id $ID" 
+    Stop-Process -Force -Id $ID
+}
+
+If(-Not($env:OSKAR_BRANCH))
+{
+    $env:OSKAR_BRANCH = "master"
+}
 If(-Not(Test-Path -PathType Container -Path "$OSKARDIR\oskar"))
 {
     git clone -b $env:OSKAR_BRANCH https://github.com/arangodb/oskar
