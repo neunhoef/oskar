eaae2057d74c9d552db4277f0a56c294d0e31a19
Frank Celler <frank@arangodb.com>
2018-11-06 05:14:42 +0100

    fixed cleanup

--- jenkins/releaseDocker.fish
+++ jenkins/releaseDocker.fish
@@ -32,9 +32,9 @@ and docker push registry.arangodb.biz:5000/arangodb/arangodb-preview:$RELEASE_TA
 and docker tag registry.arangodb.biz:5000/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY registry.arangodb.biz:5000/arangodb/linux-enterprise-maintainer:$RELEASE_TAG
 and docker push registry.arangodb.biz:5000/arangodb/linux-enterprise-maintainer:$RELEASE_TAG
 and begin
-  rm -rf $WORKSPACE/imagenames.log
-  echo arangodb/arangodb-preview:$RELEASE_TAG >> $WORKSPACE/arangodb3.docker
-  echo registry.arangodb.biz:5000/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY >> $WORKSPACE/arangodb3e.docker
+  rm -rf $WORKSPACE/*.docker
+  echo arangodb/arangodb-preview:$RELEASE_TAG > $WORKSPACE/arangodb3.docker
+  echo registry.arangodb.biz:5000/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY > $WORKSPACE/arangodb3e.docker
 end
 
 set -l s $status
