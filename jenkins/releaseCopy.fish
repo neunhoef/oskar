e2ecbc5db4a457e51ceccf5a500d98620fa89eae
Frank Celler <frank@arangodb.com>
2018-11-06 19:52:51 +0100

    fixed paths

--- jenkins/releaseCopy.fish
+++ jenkins/releaseCopy.fish
@@ -1,30 +1,33 @@
 #!/usr/bin/env fish
-rm -rf $WORKSPACE/release
-and mkdir -p $WORKSPACE/release/snippets
+set -xg SRC .
+set -xg DST .
+
+rm -rf $DST/release
+and mkdir -p $DST/release/snippets
 and for e in Community Enterprise
   for d in Linux Windows MacOSX
-    mkdir -p $WORKSPACE/release/packages/$e/$d
+    mkdir -p $DST/release/packages/$e/$d
   end
 end
 
-and mv $WORKSPACE/arangodb3[_-]*.deb $WORKSPACE/release/packages/Community/Linux
-and mv $WORKSPACE/arangodb3[_-]*.rpm $WORKSPACE/release/packages/Community/Linux
-and mv $WORKSPACE/arangodb3-linux-*.tar.gz $WORKSPACE/release/packages/Community/Linux
+and mv $SRC/arangodb3[_-]*.deb $DST/release/packages/Community/Linux
+and mv $SRC/arangodb3[_-]*.rpm $DST/release/packages/Community/Linux
+and mv $SRC/arangodb3-linux-*.tar.gz $DST/release/packages/Community/Linux
 
-and mv $WORKSPACE/arangodb3-*.dmg $WORKSPACE/release/packages/Community/MacOSX
-and mv $WORKSPACE/arangodb3-macosx-*.tar.gz $WORKSPACE/release/packages/Community/MacOSX
+and mv $SRC/arangodb3-*.dmg $DST/release/packages/Community/MacOSX
+and mv $SRC/arangodb3-macosx-*.tar.gz $DST/release/packages/Community/MacOSX
 
-and mv $WORKSPACE/ArangoDB3-*.exe $WORKSPACE/release/packages/Community/Windows
-and mv $WORKSPACE/ArangoDB3-*.zip $WORKSPACE/release/packages/Community/Windows
+and mv $SRC/ArangoDB3-*.exe $DST/release/packages/Community/Windows
+and mv $SRC/ArangoDB3-*.zip $DST/release/packages/Community/Windows
 
-and mv $WORKSPACE/arangodb3e[_-]*.deb $WORKSPACE/release/packages/Enterprise/Linux
-and mv $WORKSPACE/arangodb3e[_-]*.rpm $WORKSPACE/release/packages/Enterprise/Linux
-and mv $WORKSPACE/arangodb3e-linux-*.tar.gz $WORKSPACE/release/packages/Enterprise/Linux
+and mv $SRC/arangodb3e[_-]*.deb $DST/release/packages/Enterprise/Linux
+and mv $SRC/arangodb3e[_-]*.rpm $DST/release/packages/Enterprise/Linux
+and mv $SRC/arangodb3e-linux-*.tar.gz $DST/release/packages/Enterprise/Linux
 
-and mv $WORKSPACE/arangodb3e-*.dmg $WORKSPACE/release/packages/Enterprise/MacOSX
-and mv $WORKSPACE/arangodb3e-macosx-*.tar.gz $WORKSPACE/release/packages/Enterprise/MacOSX
+and mv $SRC/arangodb3e-*.dmg $DST/release/packages/Enterprise/MacOSX
+and mv $SRC/arangodb3e-macosx-*.tar.gz $DST/release/packages/Enterprise/MacOSX
 
-and mv $WORKSPACE/ArangoDB3e-*.exe $WORKSPACE/release/packages/Enterprise/Windows
-and mv $WORKSPACE/ArangoDB3e-*.zip $WORKSPACE/release/packages/Enterprise/Windows
+and mv $SRC/ArangoDB3e-*.exe $DST/release/packages/Enterprise/Windows
+and mv $SRC/ArangoDB3e-*.zip $DST/release/packages/Enterprise/Windows
 
-and mv $WORKSPACE/*.html $WORKSPACE/release/snippets
+and mv $SRC/*.html $DST/release/snippets
