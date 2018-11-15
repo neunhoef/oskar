c2d56f8c8717fa25ef206e3b455fdfadc6b41e38
Luschgy <34065926+Luschgy@users.noreply.github.com>
2018-11-15 16:03:29 +0100

    added push to external enterprise docker registry (#48)

--- jenkins/releaseDocker.fish
+++ jenkins/releaseDocker.fish
@@ -31,10 +31,13 @@ and makeDockerImage registry.arangodb.biz:5000/arangodb/arangodb-preview:$RELEAS
 and docker push registry.arangodb.biz:5000/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY
 and docker tag registry.arangodb.biz:5000/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY registry.arangodb.biz:5000/arangodb/linux-enterprise-maintainer:$RELEASE_TAG
 and docker push registry.arangodb.biz:5000/arangodb/linux-enterprise-maintainer:$RELEASE_TAG
+and docker tag registry.arangodb.biz:5000/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY registry-upload.arangodb.info/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY
+and docker push registry-upload.arangodb.info/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY
 and begin
   rm -rf $WORKSPACE/*.docker
   echo arangodb/arangodb-preview:$RELEASE_TAG > $WORKSPACE/arangodb3.docker
   echo registry.arangodb.biz:5000/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY > $WORKSPACE/arangodb3e.docker
+  echo registry-upload.arangodb.info/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY >> $WORKSPACE/arangodb3e.docker
 end
 
 set -l s $status
