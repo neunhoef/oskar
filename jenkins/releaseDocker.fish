a5024b4e6234ea138d3e4895b8218f174de5a229
Frank Celler <frank@arangodb.com>
2018-11-29 16:38:28 +0100

    fixed docker tag

--- jenkins/releaseDocker.fish
+++ jenkins/releaseDocker.fish
@@ -3,6 +3,12 @@ source jenkins/helper.jenkins.fish ; prepareOskar
 
 lockDirectory ; updateOskar ; clearResults
 
+set -g DOCKER_TAG $RELEASE_TAG
+
+if test (string sub --length 1 "$RELEASE_TAG") = "v"
+  set -g DOCKER_TAG (string sub --start 2 "$RELEASE_TAG")
+end
+
 community
 and maintainerOff
 and releaseMode
@@ -10,8 +16,8 @@ and switchBranches "$RELEASE_TAG" "$RELEASE_TAG" true
 and findArangoDBVersion
 and buildStaticArangoDB -DTARGET_ARCHITECTURE=nehalem
 and downloadStarter
-and makeDockerImage arangodb/arangodb-preview:$RELEASE_TAG
-and docker push arangodb/arangodb-preview:$RELEASE_TAG
+and makeDockerImage arangodb/arangodb-preview:$DOCKER_TAG
+and docker push arangodb/arangodb-preview:$DOCKER_TAG
 
 if test $status -ne 0
   echo Production of community image failed, giving up...
@@ -27,17 +33,17 @@ and findArangoDBVersion
 and buildStaticArangoDB -DTARGET_ARCHITECTURE=nehalem
 and downloadStarter
 and downloadSyncer
-and makeDockerImage registry.arangodb.biz:5000/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY
-and docker push registry.arangodb.biz:5000/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY
-and docker tag registry.arangodb.biz:5000/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY registry.arangodb.biz:5000/arangodb/linux-enterprise-maintainer:$RELEASE_TAG
-and docker push registry.arangodb.biz:5000/arangodb/linux-enterprise-maintainer:$RELEASE_TAG
-and docker tag registry.arangodb.biz:5000/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY registry-upload.arangodb.info/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY
-and docker push registry-upload.arangodb.info/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY
+and makeDockerImage registry.arangodb.biz:5000/arangodb/arangodb-preview:$DOCKER_TAG-$ENTERPRISE_DOCKER_KEY
+and docker push registry.arangodb.biz:5000/arangodb/arangodb-preview:$DOCKER_TAG-$ENTERPRISE_DOCKER_KEY
+and docker tag registry.arangodb.biz:5000/arangodb/arangodb-preview:$DOCKER_TAG-$ENTERPRISE_DOCKER_KEY registry.arangodb.biz:5000/arangodb/linux-enterprise-maintainer:$DOCKER_TAG
+and docker push registry.arangodb.biz:5000/arangodb/linux-enterprise-maintainer:$DOCKER_TAG
+and docker tag registry.arangodb.biz:5000/arangodb/arangodb-preview:$DOCKER_TAG-$ENTERPRISE_DOCKER_KEY registry-upload.arangodb.info/arangodb/arangodb-preview:$DOCKER_TAG-$ENTERPRISE_DOCKER_KEY
+and docker push registry-upload.arangodb.info/arangodb/arangodb-preview:$DOCKER_TAG-$ENTERPRISE_DOCKER_KEY
 and begin
   rm -rf $WORKSPACE/*.docker
-  echo arangodb/arangodb-preview:$RELEASE_TAG > $WORKSPACE/arangodb3.docker
-  echo registry.arangodb.biz:5000/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY > $WORKSPACE/arangodb3e.docker
-  echo registry.arangodb.com/arangodb/arangodb-preview:$RELEASE_TAG-$ENTERPRISE_DOCKER_KEY >> $WORKSPACE/arangodb3e.docker
+  echo arangodb/arangodb-preview:$DOCKER_TAG > $WORKSPACE/arangodb3.docker
+  echo registry.arangodb.biz:5000/arangodb/arangodb-preview:$DOCKER_TAG-$ENTERPRISE_DOCKER_KEY > $WORKSPACE/arangodb3e.docker
+  echo registry.arangodb.com/arangodb/arangodb-preview:$DOCKER_TAG-$ENTERPRISE_DOCKER_KEY >> $WORKSPACE/arangodb3e.docker
 end
 
 set -l s $status
