17529b99462997cc9db72128b32363f084fe4dd7
Frank Celler <frank@arangodb.com>
2018-11-30 13:39:07 +0100

    clean workspace

--- jenkins/releasePackages.fish
+++ jenkins/releasePackages.fish
@@ -1,7 +1,7 @@
 #!/usr/bin/env fish
 source jenkins/helper.jenkins.fish ; prepareOskar
 
-lockDirectory ; updateOskar ; clearResults
+lockDirectory ; updateOskar ; clearResults ; cleanWorkspace
 
 switchBranches "$RELEASE_TAG" "$RELEASE_TAG" true
 and makeRelease
