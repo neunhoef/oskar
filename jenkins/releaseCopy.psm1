769587a5c514a689dad0a3aa78fad61b467e35ca
Frank Celler <frank@arangodb.com>
2018-12-01 05:50:11 +0100

    release copy for windows

---
+++ jenkins/releaseCopy.psm1
@@ -0,0 +1,32 @@
+Copy-Item -Force "$env:WORKSPACE\jenkins\prepareOskar.ps1" $pwd
+. "$pwd\prepareOskar.ps1"
+
+If(-Not(Test-Path -PathType Container -Path release/packages/Community/Windows))
+{
+  New-Item -ItemType Directory -Path release/packages/Community/Windows
+}
+
+If(-Not(Test-Path -PathType Container -Path release/packages/Enterprise/Windows))
+{
+  New-Item -ItemType Directory -Path release/packages/Enterprise/Windows
+}
+
+ForEach($file in $(Get-ChildItem -Path $INNERWORKDIR -Filter "ArangoDB3-*.exe").fullName)
+{
+  Copy-Item "$file" -Destination "release/packages/Community/Windows"
+}
+
+ForEach($file in $(Get-ChildItem -Path $INNERWORKDIR -Filter "ArangoDB3-*.zip").fullName)
+{
+  Copy-Item "$file" -Destination "release/packages/Community/Windows"
+}
+
+ForEach($file in $(Get-ChildItem -Path $INNERWORKDIR -Filter "ArangoDB3e-*.exe").fullName)
+{
+  Copy-Item "$file" -Destination "release/packages/Enterprise/Windows"
+}
+
+ForEach($file in $(Get-ChildItem -Path $INNERWORKDIR -Filter "ArangoDB3e-*.zip").fullName)
+{
+  Copy-Item "$file" -Destination "release/packages/Enterprise/Windows"
+}
