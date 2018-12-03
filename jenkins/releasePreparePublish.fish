22c62c4ee25b2e235d7d0e70d7523ad208aea6be
Frank Celler <frank@arangodb.com>
2018-12-03 17:24:37 +0100

    fixed path

--- jenkins/releasePreparePublish.fish
+++ jenkins/releasePreparePublish.fish
@@ -5,7 +5,9 @@ lockDirectory ; updateOskar ; clearResults ; cleanWorkspace
 
 switchBranches "$RELEASE_TAG" "$RELEASE_TAG" true
 and findArangoDBVersion
-and tar -C release -c -f - $WORKSPACE/packages | tar -C $STORAGE_PATH/$ARANGODB_PACKAGES -x -f -
+and test -d $WORKSPACE/release/packages
+and test -d $STORAGE_PATH/$ARANGODB_PACKAGES
+and tar -C $WORKSPACE/release -c -f - packages | tar -C $STORAGE_PATH/$ARANGODB_PACKAGES -x -f -
 
 set -l s $status
 cd "$HOME/$NODE_NAME/$OSKAR" ; unlockDirectory
