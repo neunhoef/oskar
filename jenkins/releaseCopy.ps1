b7f1de410e8dc12468b178c54d16db7b63adde4f
Frank Celler <frank@arangodb.com>
2018-12-05 19:46:45 +0100

    copy html

--- jenkins/releaseCopy.ps1
+++ jenkins/releaseCopy.ps1
@@ -8,6 +8,11 @@ If(-Not(Test-Path -PathType Container -Path release/packages/Enterprise/Windows)
   New-Item -ItemType Directory -Path release/packages/Enterprise/Windows
 }
 
+If(-Not(Test-Path -PathType Container -Path release/snippets))
+{
+  New-Item -ItemType Directory -Path release/snippets
+}
+
 echo $pwd
 dir
 
@@ -29,4 +34,9 @@ ForEach($file in $(Get-ChildItem -Path . -Filter "ArangoDB3e-*.exe").fullName)
 ForEach($file in $(Get-ChildItem -Path . -Filter "ArangoDB3e-*.zip").fullName)
 {
   Copy-Item "$file" -Destination "release/packages/Enterprise/Windows"
+}
+
+ForEach($file in $(Get-ChildItem -Path . -Filter "download-windows-*.html").fullName)
+{
+  Copy-Item "$file" -Destination "release/snippets"
 }
