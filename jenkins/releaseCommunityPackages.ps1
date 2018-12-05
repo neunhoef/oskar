eae9b1eb38af248c2053bcbc9dfc8ca1c0d6bccd
Frank Celler <frank@arangodb.com>
2018-12-05 14:20:57 +0100

    added splitted release scripts

---
+++ jenkins/releaseCommunityPackages.ps1
@@ -0,0 +1,20 @@
+Copy-Item -Force "$env:WORKSPACE\jenkins\prepareOskar.ps1" $pwd
+. "$pwd\prepareOskar.ps1"
+
+switchBranches $env:RELEASE_TAG $env:RELEASE_TAG
+If ($global:ok) 
+{
+    makeCommunityRelease
+}
+$s = $global:ok
+moveResultsToWorkspace
+unlockDirectory
+
+If($s)
+{
+    Exit 0
+}
+Else
+{
+    Exit 1
+} 
